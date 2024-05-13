e08786c3d8f5c1f8c08a473bf9fc89bf99de1dca
Vinicius Mayrink <vmayrink12@gmail.com>
2024-05-13 19:38:57 -0300

    Fixing build errors

--- app/src/Project.js
+++ app/src/Project.js
@@ -1,5 +1,5 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { prism, dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 function Project(props) {
   return (
