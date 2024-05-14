9dd4e192aaf52b512536e70b89e9717c68f97a58
Vinicius Mayrink <vmayrink12@gmail.com>
2024-05-14 10:37:10 -0300

    Adding contact and footer sections

--- app/src/Project.js
+++ app/src/Project.js
@@ -9,7 +9,7 @@ function Project(props) {
        <SyntaxHighlighter language={props.language} style={darcula}>
          {props.codeString}
        </SyntaxHighlighter>
-       <a href={props.linkUrl} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+       <a href={props.linkUrl} class="inline-flex items-center px-3 py-2 mt-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         {props.linkDescription}
        </a>
      </div>
