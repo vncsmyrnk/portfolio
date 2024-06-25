375280078232fc7faec128372f5cdeaa524ac155
Vinicius Mayrink <vmayrink12@gmail.com>
2024-06-25 12:44:54 -0300

    Identing code samples

--- app/src/data/projects.ts
+++ app/src/data/projects.ts
@@ -4,14 +4,14 @@ export const projects = [
     description: "projects.restaurant.description",
     language: "python",
     codeString: `[...]
-    bp = Blueprint('bp_ingrediente', __name__)
+bp = Blueprint('bp_ingrediente', __name__)
 
-    @bp.get("")
-    def get_ingredientes():
-      with session_scope() as session:
-      ingredientes = session.query(Ingrediente).all()
-    return jsonify([i.serialize() for i in ingredientes])
-    [...]`,
+@bp.get("")
+def get_ingredientes():
+  with session_scope() as session:
+  ingredientes = session.query(Ingrediente).all()
+return jsonify([i.serialize() for i in ingredientes])
+[...]`,
     linkUrl: "https://github.com/Sistema-de-integracao-em-restaurante/api",
     linkDescription: "projects.button-to-github",
     tags: [
@@ -29,13 +29,13 @@ export const projects = [
     description: "projects.todayiwill.description",
     language: "rust",
     codeString: `[...]
-    let args = Cli::parse();
-    let config = Config::default();
-    match args.command {
-        Commands::Add { description, time } => {
-            let result = helper::parse_time(&time);
-            let (hour, minutes) = match result {
-    [...]`,
+let args = Cli::parse();
+let config = Config::default();
+match args.command {
+    Commands::Add { description, time } => {
+        let result = helper::parse_time(&time);
+        let (hour, minutes) = match result {
+[...]`,
     linkUrl: "https://github.com/vncsmyrnk/todayiwill",
     linkDescription: "projects.button-to-github",
     tags: [
@@ -53,15 +53,15 @@ export const projects = [
     description: "projects.work-time-calculator.description",
     language: "java",
     codeString: `[...]
-    @PostMapping(value = "day")
-    public ResponseEntity<List<Calculation>> calculateDay(@Valid @RequestBody RequestDay requestDay) {
-      Day day =
-        DayBuilder.of(
-          requestDay.getDate(), requestDay.getRegisteredRecords(), requestDay.getShiftRecords());
-          List<Calculation> calculations = day.calculate();
-          return ResponseEntity.ok(calculations);
-    }
-    [...]`,
+@PostMapping(value = "day")
+public ResponseEntity<List<Calculation>> calculateDay(@Valid @RequestBody RequestDay requestDay) {
+  Day day =
+    DayBuilder.of(
+      requestDay.getDate(), requestDay.getRegisteredRecords(), requestDay.getShiftRecords());
+      List<Calculation> calculations = day.calculate();
+      return ResponseEntity.ok(calculations);
+}
+[...]`,
     linkUrl: "https://github.com/clocked-app/calculations-api",
     linkDescription: "projects.button-to-github",
     tags: [
@@ -78,16 +78,16 @@ export const projects = [
     description: "projects.sentiment-analysis.description",
     language: "python",
     codeString: `[...]
-    @app.post("/analyze")
-    def analyze():
-      distilled_student_sentiment_classifier = pipeline(
-        model="lxyuan/distilbert-base-multilingual-cased-sentiments-student",
-          return_all_scores=True
-    )
-    print(request.data, request.json)
-    sentence_to_analyze = request.json['sentence']
-    return distilled_student_sentiment_classifier(sentence_to_analyze)
-    [...]`,
+@app.post("/analyze")
+def analyze():
+  distilled_student_sentiment_classifier = pipeline(
+    model="lxyuan/distilbert-base-multilingual-cased-sentiments-student",
+      return_all_scores=True
+)
+print(request.data, request.json)
+sentence_to_analyze = request.json['sentence']
+return distilled_student_sentiment_classifier(sentence_to_analyze)
+[...]`,
     linkUrl: "https://github.com/vncsmyrnk/sentiment-analysis-pt",
     linkDescription: "projects.button-to-github",
     tags: [
@@ -105,16 +105,16 @@ export const projects = [
     description: "projects.digital-donations.description",
     language: "javascript",
     codeString: `[...]
-    @app.post("/analyze")
-    def analyze():
-      distilled_student_sentiment_classifier = pipeline(
-        model="lxyuan/distilbert-base-multilingual-cased-sentiments-student",
-          return_all_scores=True
-    )
-    print(request.data, request.json)
-    sentence_to_analyze = request.json['sentence']
-    return distilled_student_sentiment_classifier(sentence_to_analyze)
-    [...]`,
+@app.post("/analyze")
+def analyze():
+  distilled_student_sentiment_classifier = pipeline(
+    model="lxyuan/distilbert-base-multilingual-cased-sentiments-student",
+      return_all_scores=True
+)
+print(request.data, request.json)
+sentence_to_analyze = request.json['sentence']
+return distilled_student_sentiment_classifier(sentence_to_analyze)
+[...]`,
     linkUrl: "https://github.com/doardigital/api",
     linkDescription: "projects.button-to-github",
     tags: ["rest api", "node.js", "koa.js", "docker"],
