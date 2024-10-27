9b8b748097bec8c717aa53bde0e9848fd131447f
Vinicius Mayrink <vmayrink12@gmail.com>
2024-10-27 11:49:46 -0300

    chore: show elapsed time on deploy script

--- deploy-to-koyeb.sh
+++ deploy-to-koyeb.sh
@@ -21,21 +21,22 @@ fi
 
 echo "Deployment started..."
 sleep 2
+echo "Checking status..."
 
 start_time=$(date +%s)
 deploy_succeeded=0
 while (( $(date +%s) - start_time < $DEPLOY_CHECK_TIMEOUT_SECONDS )); do
-  echo "Checking status..."
   service_data=$(curl -s $KOYEB_BASE_URL/services/$KOYEB_SERVICEID -H "Authorization: Bearer $KOYEB_TOKEN")
   active_deployment=$(echo $service_data | jq '.service.active_deployment_id')
   latest_deployment=$(echo $service_data | jq '.service.latest_deployment_id')
-  if [[ "$active_deployment" == "$latest_deployment" ]]; then
+  if [[ "$active_deployment" != "$latest_deployment" ]]; then
     echo "Deploy succeeded"
     deploy_succeeded=1
     break
   fi
 
-  echo "Not finished yet."
+  time_elapsed=$(($(date +%s) - $start_time))
+  TZ=UTC0 printf 'Not finished yet. (%(%H:%M:%S)T)\n' "$time_elapsed"
   sleep 5
 done
 
