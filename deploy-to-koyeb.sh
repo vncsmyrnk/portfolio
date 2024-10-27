20c4c5f5645742f531457d5b3819b365ef6b7f5a
Vinicius Mayrink <vmayrink12@gmail.com>
2024-10-27 12:05:25 -0300

    chore: changes finish check

--- deploy-to-koyeb.sh
+++ deploy-to-koyeb.sh
@@ -29,7 +29,7 @@ while (( $(date +%s) - start_time < $DEPLOY_CHECK_TIMEOUT_SECONDS )); do
   service_data=$(curl -s $KOYEB_BASE_URL/services/$KOYEB_SERVICEID -H "Authorization: Bearer $KOYEB_TOKEN")
   active_deployment=$(echo $service_data | jq '.service.active_deployment_id')
   latest_deployment=$(echo $service_data | jq '.service.latest_deployment_id')
-  if [[ "$active_deployment" != "$latest_deployment" ]]; then
+  if [[ "$active_deployment" == "$latest_deployment" ]]; then
     echo "Deploy succeeded"
     deploy_succeeded=1
     break
