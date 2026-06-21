e642142afa87055832065efdb3ea40feefea0639
Vinicius Mayrink <vmayrink12@gmail.com>
2026-06-21 17:57:45 -0300

    fix(font): drops broken ubuntu font dependency for the Google Fonts solution
    sandbox check worked:
    https://github.com/vncsmyrnk/portfolio/actions/runs/27917262317
    
    resolves #95

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@fontsource/ubuntu/index.css";
 import "./index.css";
 import "./i18n/config";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
